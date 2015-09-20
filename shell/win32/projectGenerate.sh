728515179efdb4e113fee8de9beeb75b3e3d004c
dorgon <dorgonman@hotmail.com>
2015-09-20 18:00:51 +0800

    adjust project generator

--- shell/win32/projectGenerate.sh
+++ shell/win32/projectGenerate.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 set -e
-pwd
+
+
+if [ "${BOOST_ROOT}" == "" ]
+then
+  BOOST_ROOT=$1
+fi
+
+if [ "${BOOST_ROOT}" == "" ]
+then
+  echo "Please define BOOST_ROOT path in system environment"
+  exit 1
+fi
+
+
+echo {BOOST_ROOT}:  ${BOOST_ROOT}
+
 #NOTE: please install msysgit inorder to execute linux shell under windows
 #rm -rf ../proj.win32
-BOOST_ROOT="$(pwd)/../../thirdparty/boost_1_59_0/"
+#BOOST_ROOT="$(pwd)/../../thirdparty/boost_1_59_0/"
 mkdir ../../proj.win32 || true
 cd ../../proj.win32
 cmake ../ -G "Visual Studio 12 2013" \
